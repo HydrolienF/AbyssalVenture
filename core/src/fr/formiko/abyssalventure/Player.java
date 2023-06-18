f792e4787cd0da3c465be987d5ad0e8d30cc4083
HydrolienF <hydrolien.f@gmail.com>
2023-06-18 14:59:06 +0200

    player

---
+++ core/src/fr/formiko/abyssalventure/Player.java
@@ -0,0 +1,16 @@
+package fr.formiko.abyssalventure;
+
+public class Player {
+    private Creature creature;
+    private int score;
+
+    public Player() {
+        creature = new Fish();
+        score = 0;
+    }
+
+    public Creature getCreature() { return creature; }
+    public void setCreature(Creature creature) { this.creature = creature; }
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
+}
